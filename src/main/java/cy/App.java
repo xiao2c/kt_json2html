5524b435792331b708d7182619afc9c73b561b70
chenyi <chenyi2x@gmail.com>
2019-01-23 17:59:09 +0800

    minor

--- src/main/java/cy/App.java
+++ src/main/java/cy/App.java
@@ -1,7 +1,5 @@
 package cy;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -9,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
